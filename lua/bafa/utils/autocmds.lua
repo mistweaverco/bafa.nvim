a7de942f65efc2d1ed2821ac6532859dee654a59
Marco Kellershoff <marco@mistweaverco.com>
2026-05-04 09:05:47 +0200

    fix(autocmds): Trailing char error in nvim-0.13+

--- lua/bafa/utils/autocmds.lua
+++ lua/bafa/utils/autocmds.lua
@@ -1,17 +1,28 @@
 local M = {}
 
 function M.defaults(bufnr)
-  vim.cmd(string.format("autocmd BufWriteCmd <buffer=%s> lua require('bafa.ui').on_menu_save()", bufnr))
-  -- replace deprecated BufModifiedSet with Optionset modified:
+  vim.api.nvim_create_autocmd("BufWriteCmd", {
+    buffer = bufnr,
+    callback = function() require("bafa.ui").on_menu_save() end,
+  })
+  -- Use OptionSet modified instead of BufModifiedSet for neovim 0.12 and above
   -- https://github.com/mistweaverco/bafa.nvim/issues/59
   -- https://github.com/neovim/neovim/commit/443171328531e33a7caecda0b81dadd826518a58
   -- neovim 0.12+ deprecated BufModifiedSet, use Optionset modified instead
-  if vim.fn.has("nvim-0.12") == 1 then -- reports 1 if nvim version is 0.12 or higher
-    vim.cmd(string.format("autocmd OptionSet modified <buffer=%s> set nomodified", bufnr))
-  else
-    vim.cmd(string.format("autocmd BufModifiedSet <buffer=%s> set nomodified", bufnr))
-  end
-  vim.cmd("autocmd BufLeave <buffer> ++nested ++once silent lua require('bafa.ui').toggle()")
+  local event = vim.fn.has("nvim-0.12") == 1 and "OptionSet" or "BufModifiedSet"
+  local opts = { buffer = bufnr }
+  vim.api.nvim_create_autocmd(
+    event,
+    vim.tbl_extend("force", opts, {
+      command = "set nomodified",
+    })
+  )
+  vim.api.nvim_create_autocmd("BufLeave", {
+    buffer = bufnr,
+    nested = true,
+    once = true,
+    callback = function() require("bafa.ui").toggle() end,
+  })
 end
 
 return M
